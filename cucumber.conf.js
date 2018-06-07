26f76a7ac36e6225f76f72cc4c0e7750f932dd9e
Ramon Medeiros <ramonmedeiros@ciandt.com>
2018-06-07 12:49:51 -0300

    Support reports after run
    Signed-off-by: Ramon Medeiros <ramonmedeiros@ciandt.com>

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -1,9 +1,9 @@
 exports.config = { 
     cucumberOpts: {
         require: 'features/step_definitions/*.js',
-        tags: false,
         profile: false,
         'no-source': true,
+        format: ['json:results.json'],
     },
     baseUrl: 'http://juliemr.github.io/',
     seleniumAddress: "http://localhost:4444/wd/hub",
@@ -14,5 +14,18 @@ exports.config = {
     },
     specs: [
         "features/*.feature"
-    ],   
+    ],
+
+    afterLaunch: function () {
+        var reporter = require('cucumber-html-reporter');
+        var options = {
+            theme: 'bootstrap',
+            jsonFile: 'results.json',
+            output: 'tests_result/cucumber_report.html',
+            storeScreenshots: true,
+            reportSuiteAsScenarios: true,
+            launchReport: true,
+        };
+        reporter.generate(options);
+    },
 };
